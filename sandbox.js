365a121df92cf1fa8a7adfce5a9430fa3f4889b3
Stanley <homestanrunner@gmail.com>
2024-07-05 21:05:52 -0400

    Tutorial Complete

---
+++ sandbox.js
@@ -0,0 +1,29 @@
+//explicit types
+var character;
+var age;
+var isLoggedIn;
+// age = 'luigi'
+age = 30;
+// isLoggedIn = 25;
+isLoggedIn = true;
+// arrays
+var ninjas = [];
+ninjas.push('yoshi');
+// union types
+var mixed = [];
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+console.log(mixed);
+var uid;
+uid = '123';
+uid = 123;
+// objects
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+var ninjaTwo;
+ninjaTwo = {
+    name: 'mario',
+    age: 20,
+    beltColour: 'black'
+};
---
+++ sandbox.ts
@@ -0,0 +1,42 @@
+//explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+// age = 'luigi'
+age = 30;
+
+// isLoggedIn = 25;
+isLoggedIn = true
+
+// arrays
+let ninjas: string[] = [];
+
+ninjas.push('yoshi');
+
+// union types
+let mixed: (string|number|boolean)[] = []
+mixed.push('hello');
+mixed.push(20)
+mixed.push(false)
+console.log(mixed)
+
+let uid: string|number;
+uid = '123';
+uid = 123;
+
+// objects
+let ninjaOne: object;
+ninjaOne = { name: 'yoshi', age: 30 };
+
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColour: string,
+}
+
+ninjaTwo = {
+  name: 'mario',
+  age: 20,
+  beltColour: 'black'
+}
