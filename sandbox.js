9077179d8e3947309dd919ef3ab49ab498113ab9
Stanley <homestanrunner@gmail.com>
2024-07-05 20:56:35 -0400

    Tutorial Complete

--- sandbox.js
+++ sandbox.js
@@ -1,37 +1,29 @@
 // arrays
-let names = ['luigi', 'mario', 'yoshi'];
-
+var names = ['luigi', 'mario', 'yoshi'];
 names.push('toad');
 // names.push(3);
 // names[0] = 3
-
-let numbers = [10, 20, 30, 40];
-
+var numbers = [10, 20, 30, 40];
 numbers.push(25);
 // numbers.push('shaun');
 // numbers[1] = 'shaun'
-
-let mixed = [ 'ken', 4, 'chun-li', 8, 9];
-
+var mixed = ['ken', 4, 'chun-li', 8, 9];
 mixed.push('ryu');
 mixed.push(10);
 mixed[0] = 3;
-
 // objects
-let ninja = {
-  name: 'mario',
-  belt: 'black',
-  age: 30
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-
 ninja.age = 40;
 ninja.name = 'ryu';
 // ninja.age = '30'
 // ninja.skills = ['fighting', 'sneaking']
-
 ninja = {
-  name: 'yoshi',
-  belt: 'orange',
-  age: 40
-  // skills: []
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+    // skills: []
 };
